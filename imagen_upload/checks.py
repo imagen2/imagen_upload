6c0249b7db5ca344c26a93546fe8def039213b97
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2016-12-27 20:29:39 +0200

    Fix HTML bug

--- imagen_upload/checks.py
+++ imagen_upload/checks.py
@@ -78,7 +78,7 @@ def get_message_error(errors, filename, pattern, filepath):
                 if len(sample) > err._SAMPLE_LEN:
                     sample = sample[:err._SAMPLE_LEN] + '...'
                 message += u' [{}]'.format(sample)
-            message += u'<dd>'
+            message += u'</dd>'
         message += u'</dl>'
     return message.replace(os.path.basename(filepath), filename)
 
